321d34d8ab8e30901002369b43e64c9d72845f80
Oleksandr Kuriachyi <a.p.kuryachiy@gmail.com>
2022-06-24 21:32:27 +0300

    initial commit

---
+++ MyFirstProgram/src/MyFirstProgram.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {   
+		
+//		System.out.println("What is your name?");
+		
+//		Scanner in = new Scanner(System.in);
+//		String name = in.nextLine();
+//		
+//		System.out.println("Hello " + name);
+		
+//		int x = 5; //primitive
+//		Integer y = 5; //object (5 is autoboxed)
+//		
+//		double z = 5.5;
+//		
+//		System.out.println(x);
+//		System.out.println(y);
+//		System.out.println(z);
+		
+//		System.out.println("Enter your name: ");
+//		
+//		Scanner scanner = new Scanner(System.in);
+//		String name = scanner.nextLine();
+//		
+//		if(name.equals("Oleksandr")) {
+//			System.out.println("Hello Oleksandr!");
+//		} else if(name.equals("Tatiana")) {
+//			System.out.println("Hi there as well!");
+//		} else {
+//			System.out.println("You're not welcome here");
+//		}
+//		
+//		scanner.close();
+		
+//		int i = 0; //initialization
+//		while(i < 10) { //condition
+//			System.out.println("i is: " + i);
+//			i++; // update
+//		}
+		
+//		for(int i = 0; i <= 10; i++) {
+//			System.out.println(i);
+//		}
+		
+		int i = 0;
+		
+		do {
+			System.out.println("i is: " + i);
+			i++;
+		} while(i < 10);
+	}
+
+}
